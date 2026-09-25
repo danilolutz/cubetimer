c4264f9040f40ffee45811f28a683008adf28b68
Danilo Lutz <danilolutz@proton.me>
2026-09-25 16:31:52 -0300

    release: prepare F-Droid compatible 1.0.2 APK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,6 +23,11 @@ val enableAbiSplits = providers.gradleProperty("enableAbiSplits")
 
 android {
     namespace = "com.danilolutz.cubetimer"
+
+    dependenciesInfo {
+        includeInApk = false
+        includeInBundle = false
+    }
     compileSdk {
         version = release(37)
     }
@@ -31,8 +36,8 @@ android {
         applicationId = "com.danilolutz.cubetimer"
         minSdk = 24
         targetSdk = 37
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
